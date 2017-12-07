7aac7664735cd982eb8408367d21cbd579724167
Alex Walker <amlwwalker@gmail.com>
2017-12-07 12:57:25 +0000

    latest changes, and wine data

--- README.md
+++ README.md
@@ -1,3 +1,19 @@
 # Simple KMeans implemented from scratch
 
 Ipython notebook outlining how kmeans works, and with a second ipython notebook that requires the user to fill in the algorithm to calculate the distance between each point and each cluster center, using pythagoras' theorem.
+
+
+
+## Instructions:
+
+
+```
+sudo pip install jupyter &&
+pip install numpy &&
+pip install pandas &&
+pip install random &&
+pip install matplotlib &&
+git clone https://github.com/amlwwalker/kmeans-ipython.git && cd kmeans-ipython
+jupyter notebook
+```
+(open `model.ipynb` for teacher, and `exercise.ipynb` for students)
--- graphLibrary.py
+++ graphLibrary.py
@@ -1,21 +1,21 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 import matplotlib.pylab as pylab
-
+from random import randint
 
 """
 Function sets each cluster center to the average X and Y of the points in its cluster
 """
 def updateClusterCenterPositions(dataset, clusterDF, column1, column2): #in jupyter this seems to fail
     #now for each cluster calculate a new X and Y based on the points in the cluster
-    clusterDF.loc[1]["CX"] = dataset.loc[dataset['cluster'] == "C1"][column1].mean()
-    clusterDF.loc[1]["CY"] = dataset.loc[dataset['cluster'] == "C1"][column2].mean()
+    clusterDF.loc[1,"CX"] = dataset.loc[dataset['cluster'] == "C1"][column1].mean()
+    clusterDF.loc[1,"CY"] = dataset.loc[dataset['cluster'] == "C1"][column2].mean()
     
-    clusterDF.loc[2]["CX"] = dataset.loc[dataset['cluster'] == "C2"][column1].mean()
-    clusterDF.loc[2]["CY"] = dataset.loc[dataset['cluster'] == "C2"][column2].mean()
+    clusterDF.loc[2,"CX"] = dataset.loc[dataset['cluster'] == "C2"][column1].mean()
+    clusterDF.loc[2,"CY"] = dataset.loc[dataset['cluster'] == "C2"][column2].mean()
     
-    clusterDF.loc[3]["CX"] = dataset.loc[dataset['cluster'] == "C3"][column1].mean()
-    clusterDF.loc[3]["CY"] = dataset.loc[dataset['cluster'] == "C3"][column2].mean()
+    clusterDF.loc[3,"CX"] = dataset.loc[dataset['cluster'] == "C3"][column1].mean()
+    clusterDF.loc[3,"CY"] = dataset.loc[dataset['cluster'] == "C3"][column2].mean()
     
     
 """
@@ -38,11 +38,19 @@ Sets the initial cluster centers to random points within the dataset
 """
 def createRandomClusterCenters(dataset, range1, range2, clusters=3):
     #create a list to store the cluster centers
+
+    ran1a = dataset[range1].iloc[randint(0, dataset.count()[0])]
+    ran1b = dataset[range1].iloc[randint(0, dataset.count()[0])]
+    ran1c = dataset[range1].iloc[randint(0, dataset.count()[0])]
+    ran2a = dataset[range2].iloc[randint(0, dataset.count()[0])]
+    ran2b = dataset[range2].iloc[randint(0, dataset.count()[0])]
+    ran2c = dataset[range2].iloc[randint(0, dataset.count()[0])]
+    
     clusterDF = pd.DataFrame(columns=["Name", "CX", "CY"])
     #manually create some clusters and store them in the list
-    clusterDF.loc[1] = ["C1", dataset[range1].iloc[7], dataset[range2].iloc[5]]
-    clusterDF.loc[2] = ["C2", dataset[range1].iloc[10], dataset[range2].iloc[15]]
-    clusterDF.loc[3] = ["C3", dataset[range1].iloc[15], dataset[range2].iloc[20]]
+    clusterDF.loc[1] = ["C1", ran1a, ran2a]
+    clusterDF.loc[2] = ["C2", ran1b, ran2b]
+    clusterDF.loc[3] = ["C3", ran1c, ran2c]
     return clusterDF
 
 
